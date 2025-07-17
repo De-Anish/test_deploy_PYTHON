97276188e54cdff91a0516f3d08451ac405ec87f
Anish DE <140321809+De-Anish@users.noreply.github.com>
2025-07-18 01:28:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify,request
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+@app.route('/calculate',methods=['POST'])
+def calculate():
+    data=request.get_json()
+    if not data or 'a' not in data or 'b' not in data or 'operation' not in data :
+        return jsonify({'error ' : ' Missing any parameters'})
+
+    a=data['a']
+    b=data['b']
+    op=data['operation']
+
+    try:
+        a=float(a)
+        b=float(b)
+    except ValueError:
+        return jsonify({'error' : 'Input must be numbers'})
+
+    if op=='+':
+        result=a+b
+    elif op=='-':
+        result=a-b
+    elif op=='*':
+        result=a*b
+    elif op=='/':
+        if b==0:
+            return jsonify({'error' : 'Cannot devided by 0'}) 
+        result = a/b 
+
+    else: 
+        return jsonify({'Error':'Invalid operation'})
+    
+    return jsonify({'Result': result})
+
+if __name__=='__main__' :
+    app.run(debug=True)
+       
