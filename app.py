9b771832cf7acfb4c77d3717c92538717028e9e7
Anish DE <140321809+De-Anish@users.noreply.github.com>
2025-07-18 01:45:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-app=Flask(__name__)
-CORS(app)
-@app.route('/calculate',methods=['POST'])
-def calculate():
-    data=request.get_json()
-    if not data or 'a' not in data or 'b' not in data or 'operation' not in data :
-        return jsonify({'error ' : ' Missing any parameters'})
+import os
 
-    a=data['a']
-    b=data['b']
-    op=data['operation']
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.get_json()
+    if not data or 'a' not in data or 'b' not in data or 'operation' not in data:
+        return jsonify({'error': 'Missing any parameters'})
+
+    a = data['a']
+    b = data['b']
+    op = data['operation']
 
     try:
-        a=float(a)
-        b=float(b)
+        a = float(a)
+        b = float(b)
     except ValueError:
-        return jsonify({'error' : 'Input must be numbers'})
+        return jsonify({'error': 'Input must be numbers'})
 
-    if op=='+':
-        result=a+b
-    elif op=='-':
-        result=a-b
-    elif op=='*':
-        result=a*b
-    elif op=='/':
-        if b==0:
-            return jsonify({'error' : 'Cannot devided by 0'}) 
-        result = a/b 
+    if op == '+':
+        result = a + b
+    elif op == '-':
+        result = a - b
+    elif op == '*':
+        result = a * b
+    elif op == '/':
+        if b == 0:
+            return jsonify({'error': 'Cannot divide by 0'})
+        result = a / b
+    else:
+        return jsonify({'error': 'Invalid operation'})
 
-    else: 
-        return jsonify({'Error':'Invalid operation'})
-    
-    return jsonify({'Result': result})
+    return jsonify({'result': result})
 
-if __name__=='__main__' :
-    app.run(debug=True)
-       
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
